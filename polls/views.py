eb8d6044bac865be4162d1952ec6f19865bf3b79
AriLeGrand <120755525+AriLeGrand@users.noreply.github.com>
2023-12-01 12:12:56 +0100

    Add files via upload

---
+++ polls/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def read_file(filename):
+    with open(filename) as f:
+        return f.read()
+
+def index(request):
+    return HttpResponse(read_file("index.html"))
