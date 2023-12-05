ba10427f80e80ccd2a99e553a85b785f972b7c62
AriLeGrand <120755525+AriLeGrand@users.noreply.github.com>
2023-12-05 23:53:32 +0100

    Add files via upload

---
+++ Myapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    # path("profil/", views.profil, name="profil"),
+    # path("login/", views.login, name="login"),
+    # path("logout/", views.logout, name="logout"),
+    # path("register", views.register, name="register"),
+    path('admin/', admin.site.urls),
+    path("profils/", include("profils.urls")),
+]
