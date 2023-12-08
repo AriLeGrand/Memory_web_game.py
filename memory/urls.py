1d3cb10e98f08966eec4ba6910540338e28f0fc6
AriLeGrand <120755525+AriLeGrand@users.noreply.github.com>
2023-12-08 06:56:40 +0100

    Add files via upload

--- memory/urls.py
+++ memory/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("memory", views.index, name="memory"),
+    path("memory/ParseGameResults", views.ParseGameResults, name="ParseGameResults"),
 ]
 
 
--- memory/views.py
+++ memory/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from profils.models import Leaderboard, HistoricalScore
+
 
 # Create your views here.
 def index(request):
     return render(request,'memory\\memory.html')
+
+def ParseGameResults(request):
+    Leaderboard(user=request.user, score=request.POST.get("totalPoints", None)).save()
+    HistoricalScore(user=request.user, score=request.POST.get("totalPoints", None)).save()
+    return render(request,'memory\\memory.html')
+    
